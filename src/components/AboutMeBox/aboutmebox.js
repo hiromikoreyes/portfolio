8c55791916eb43b637532b14fcb92c1f59bfa3b9
hiromikoreyes <114784445+hiromikoreyes@users.noreply.github.com>
2023-09-26 21:00:56 -0400

    added a better message

--- src/components/AboutMeBox/aboutmebox.js
+++ src/components/AboutMeBox/aboutmebox.js
@@ -7,7 +7,7 @@ function AboutMeBox(){
             <div class="about-container">
                 <div class="left-column">
                     <div class="img-container">
-                        <img src="https://media.licdn.com/dms/image/D5635AQHNSlSu-_GyPA/profile-framedphoto-shrink_200_200/0/1695763277026?e=1696377600&v=beta&t=n2j7tVNedWO4YRPdxnaRcO7mS6FenhU9vLGsItyq_j8"/>
+                        <img src="https://media.licdn.com/dms/image/D5603AQHxcNZL5fPAjw/profile-displayphoto-shrink_800_800/0/1695763275265?e=1701302400&v=beta&t=Psdk6T3fg7iuuS_EBXE86bsk5e20s2rOFCCw2OXEyrg"/>
                     </div>
 
                     <div class="below-img-container">
@@ -22,17 +22,23 @@ function AboutMeBox(){
                     <div class="paragraph">
                         <p>
                         Hi, I'm a <a style={{fontWeight: 500}}>Computer Science Specialist</a> and Music Minor at the
-                        <a style={{fontWeight: 500}}> University Of Toronto</a>. 
+                        <a style={{fontWeight: 500}}> University Of Toronto</a>. My academic pursuits have allowed me
+                        to delve into the fascinating world of technology while also developing my passion for music.
+                        
+                        </p>
+                        <br></br>
+                        <p>
+                        CompSci-wise, I'm really interested in front-end, simulations, cool projects, and learning new things.
+                        So far I've worked with: React, Flask, Android Studio and ... [you should check out my resume for the rest ;) ]
+                        </p>
+                        <br></br>
+                        <p style={{textAlign: "center"}}>
+                        [MUSIC-WISE ... to be continued ...]
                         </p>
 
                         <br></br>
-
                         <p>
-                            I love learning things please give me a job!!!! please puh-leassee!!!
-                            I love learning things please give me a job!!!! please puh-leassee!!!
-                            I love learning things please give me a job!!!! please puh-leassee!!!
-                            I love learning things please give me a job!!!! please puh-leassee!!!
-                            I love learning things please give me a job!!!! please puh-leassee!!!
+                        If you're interested in contacting me, I'd be delighted to connect and collaborate!
                         </p>
 
 
