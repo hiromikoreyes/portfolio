ea5372005c7405f1637acfe3facae7ec5d35a220
hiromikoreyes <[200~114784445+hiromikoreyes@users.noreply.github.com>
2024-02-03 17:33:22 -0500

    redo base

--- README.md
+++
@@ -1,7 +0,0 @@
-# FIRST REACT PROJECT
-## GOALS
-- Have a 3D monkey that rotates in the z-axis
-- display some github repos
-- display favourite music
-
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
---
+++ frontend/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    
+      <div className="font-mono text-4xl m-20">
+      MIKO REYES | 2nd Year CS & MUSIC @ UOFT
+      </div>
+
+
+
+      <div style={{witdh: "1280px"}}>
+
+      BIO
+
+      </div>
+
+
+    </>
+  )
+}
+
+export default App
