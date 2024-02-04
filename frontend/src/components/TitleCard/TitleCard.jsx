605874991e85de471d26a062d4a312ebc8719249
hiromikoreyes <114784445+hiromikoreyes@users.noreply.github.com>
2024-02-04 08:07:06 -0500

    added image

--- frontend/src/components/TitleCard/TitleCard.jsx
+++ frontend/src/components/TitleCard/TitleCard.jsx
@@ -6,15 +6,15 @@ import SkipArrow from '../SkipArrow/SkipArrow'
 
 function TitleCard() {
 
-  return (
-    <>
+  return ( 
+    <div className="backdrop-blur-3xl city-bg" style={{backgroundImage: `url('https://assets.playgroundai.com/985d8118-90c7-4695-ab4f-aa9300316346.jpg')`}}>
     
     <marquee className="m-0 text-white" scrollamount="50" behavior="scroll" direction="left">~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~</marquee>
 
       <div className="font-sans font-thin text-6xl mt-24 text-white">
       MIKO REYES
       </div>
-      <div className="info-text font-sans font-thin text-sm text-white hover:animate-pulse">
+      <div className="info-text font-sans font-thin text-sm text-white">
       </div>
 
       <SkipArrow></SkipArrow>
@@ -25,7 +25,7 @@ function TitleCard() {
       </div>
 
 
-    </>
+    </div>
   )
 }
 
