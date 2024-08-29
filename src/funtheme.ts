541eeebe401fe66ec75fff08cffe1b8011da8055
hiromikoreyes <114784445+hiromikoreyes@users.noreply.github.com>
2024-08-29 10:39:06 -0400

    idk

---
+++ src/funtheme.ts
@@ -0,0 +1,101 @@
+import type { CustomThemeConfig } from '@skeletonlabs/tw-plugin';
+
+export const funtheme: CustomThemeConfig = {
+	name: 'funtheme',
+	properties: {
+		// =~= Theme Properties =~=
+		'--theme-font-family-base': `system-ui`,
+		'--theme-font-family-heading': `system-ui`,
+		'--theme-font-color-base': '0 0 0',
+		'--theme-font-color-dark': '255 255 255',
+		'--theme-rounded-base': '9999px',
+		'--theme-rounded-container': '8px',
+		'--theme-border-base': '1px',
+		// =~= Theme On-X Colors =~=
+		'--on-primary': '0 0 0',
+		'--on-secondary': '0 0 0',
+		'--on-tertiary': '0 0 0',
+		'--on-success': '0 0 0',
+		'--on-warning': '255 255 255',
+		'--on-error': '0 0 0',
+		'--on-surface': '0 0 0',
+		// =~= Theme Colors  =~=
+		// primary | #a1fc82
+		'--color-primary-50': '241 255 236', // #f1ffec
+		'--color-primary-100': '236 254 230', // #ecfee6
+		'--color-primary-200': '232 254 224', // #e8fee0
+		'--color-primary-300': '217 254 205', // #d9fecd
+		'--color-primary-400': '189 253 168', // #bdfda8
+		'--color-primary-500': '161 252 130', // #a1fc82
+		'--color-primary-600': '145 227 117', // #91e375
+		'--color-primary-700': '121 189 98', // #79bd62
+		'--color-primary-800': '97 151 78', // #61974e
+		'--color-primary-900': '79 123 64', // #4f7b40
+		// secondary | #7dcb23
+		'--color-secondary-50': '236 247 222', // #ecf7de
+		'--color-secondary-100': '229 245 211', // #e5f5d3
+		'--color-secondary-200': '223 242 200', // #dff2c8
+		'--color-secondary-300': '203 234 167', // #cbeaa7
+		'--color-secondary-400': '164 219 101', // #a4db65
+		'--color-secondary-500': '125 203 35', // #7dcb23
+		'--color-secondary-600': '113 183 32', // #71b720
+		'--color-secondary-700': '94 152 26', // #5e981a
+		'--color-secondary-800': '75 122 21', // #4b7a15
+		'--color-secondary-900': '61 99 17', // #3d6311
+		// tertiary | #7daba4
+		'--color-tertiary-50': '236 242 241', // #ecf2f1
+		'--color-tertiary-100': '229 238 237', // #e5eeed
+		'--color-tertiary-200': '223 234 232', // #dfeae8
+		'--color-tertiary-300': '203 221 219', // #cbdddb
+		'--color-tertiary-400': '164 196 191', // #a4c4bf
+		'--color-tertiary-500': '125 171 164', // #7daba4
+		'--color-tertiary-600': '113 154 148', // #719a94
+		'--color-tertiary-700': '94 128 123', // #5e807b
+		'--color-tertiary-800': '75 103 98', // #4b6762
+		'--color-tertiary-900': '61 84 80', // #3d5450
+		// success | #e0acae
+		'--color-success-50': '250 243 243', // #faf3f3
+		'--color-success-100': '249 238 239', // #f9eeef
+		'--color-success-200': '247 234 235', // #f7eaeb
+		'--color-success-300': '243 222 223', // #f3dedf
+		'--color-success-400': '233 197 198', // #e9c5c6
+		'--color-success-500': '224 172 174', // #e0acae
+		'--color-success-600': '202 155 157', // #ca9b9d
+		'--color-success-700': '168 129 131', // #a88183
+		'--color-success-800': '134 103 104', // #866768
+		'--color-success-900': '110 84 85', // #6e5455
+		// warning | #2c5ea1
+		'--color-warning-50': '223 231 241', // #dfe7f1
+		'--color-warning-100': '213 223 236', // #d5dfec
+		'--color-warning-200': '202 215 232', // #cad7e8
+		'--color-warning-300': '171 191 217', // #abbfd9
+		'--color-warning-400': '107 142 189', // #6b8ebd
+		'--color-warning-500': '44 94 161', // #2c5ea1
+		'--color-warning-600': '40 85 145', // #285591
+		'--color-warning-700': '33 71 121', // #214779
+		'--color-warning-800': '26 56 97', // #1a3861
+		'--color-warning-900': '22 46 79', // #162e4f
+		// error | #94c936
+		'--color-error-50': '239 247 225', // #eff7e1
+		'--color-error-100': '234 244 215', // #eaf4d7
+		'--color-error-200': '228 242 205', // #e4f2cd
+		'--color-error-300': '212 233 175', // #d4e9af
+		'--color-error-400': '180 217 114', // #b4d972
+		'--color-error-500': '148 201 54', // #94c936
+		'--color-error-600': '133 181 49', // #85b531
+		'--color-error-700': '111 151 41', // #6f9729
+		'--color-error-800': '89 121 32', // #597920
+		'--color-error-900': '73 98 26', // #49621a
+		// surface | #8aaecb
+		'--color-surface-50': '237 243 247', // #edf3f7
+		'--color-surface-100': '232 239 245', // #e8eff5
+		'--color-surface-200': '226 235 242', // #e2ebf2
+		'--color-surface-300': '208 223 234', // #d0dfea
+		'--color-surface-400': '173 198 219', // #adc6db
+		'--color-surface-500': '138 174 203', // #8aaecb
+		'--color-surface-600': '124 157 183', // #7c9db7
+		'--color-surface-700': '104 131 152', // #688398
+		'--color-surface-800': '83 104 122', // #53687a
+		'--color-surface-900': '68 85 99' // #445563
+	}
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,57 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang>
+	const experienceItems = [
+		{
+			employer: 'Infrastructure For Information Inc.',
+			job: 'Software Developer',
+			location: 'Toronto (Hybrid)',
+			time: 'May 2024 - Aug 2025',
+			description: 'doing coop intern things',
+			image: '',
+			link: 'https://www.i4i.com/'
+		},
+		{
+			employer: 'Iki Design Studios (Riipen Level Up Program)',
+			job: 'Full Stack Developer',
+			location: 'Remote',
+			time: 'May 2024 - Jun 2024',
+			link: 'https://levelup.riipen.com/users/4LA7MbBO',
+			description: 'made react stuff',
+			image: ''
+		},
+		{
+			employer: 'Seafood City',
+			job: 'Cashier',
+			location: 'Scarborough',
+			time: 'Apr 2022 - Aug 2022',
+			link: 'https://www.seafoodcity.com/',
+			description: 'cashed out ppl',
+			image: ''
+		}
+	];
+</script>
+
+<div class="flex flex-col items-center w-screen">
+	<div class="xl:max-w-screen-xl max-w-screen-sm">
+		<div class="text-2xl text-center p-12">
+			hi. im <span class="text-6xl ml-8">Hiro Miko Reyes</span>.<br />
+			i like coding and music. <br />
+			this is a barebones portfolio website.<br />
+		</div>
+
+		<div class="underline text-3xl">experience</div>
+		{#each experienceItems as item, i}
+			<div class="flex flex-col card px-4 py-8 m-4">
+				<a href={item.link} class="text-2xl hover:text-primary-600 hover:animate-pulse ease-in">{item.employer}</a>
+				{item.job} <br />
+				{item.time} <br />
+				{item.location} <br />
+				<hr class="my-2" />
+				<p class="text-center">
+					{item.description} <br />
+				</p>
+			</div>
+		{/each}
+
+		<div class="underline text-3xl">projects</div>
+	</div>
+</div>
